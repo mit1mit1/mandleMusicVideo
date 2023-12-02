a696e7288bb5f84ebe44c167d458c5169f0ae768
Mitchell Roberts <mitchell.roberts11@gmail.com>
2023-12-03 10:35:34 +1100

    start dr who

--- aubioScripts/getOnsets.sh
+++ aubioScripts/getOnsets.sh
@@ -1,4 +1,4 @@
-aubio onset "./input/testPiece1.m4a" -t 0.6 -s -45 > rhythmInstrument1Onsets.txt
-aubio notes "./input/testPiece1.m4a" -s -45 > pitchedInstrument1NotesDirty.txt
+aubio onset "./input/drWhoBassOnly.mp3" -t 0.6 -s -45 > rhythmInstrument1Onsets.txt
+aubio notes "./input/drWhoMelodiesOnly.mp3" -s -45 > pitchedInstrument1NotesDirty.txt
 # Notes file includes some notes without timestamps oddly
 sed -e 's/[^[:space:][:cntrl:]]/&/15' -e t -e d < pitchedInstrument1NotesDirty.txt > pitchedInstrument1Notes.txt
--- colors.cpp
+++ colors.cpp
@@ -3,64 +3,52 @@
 #include <vector>
 
 std::vector<PixelColor> getColors() {
-  std::vector<PixelColor> rollingInTheDeepColors = {
-      PixelColor{.red = 232, .green = 208, .blue = 197, .alpha = 255},
-      PixelColor{.red = 208, .green = 173, .blue = 167, .alpha = 255},
-      PixelColor{.red = 173, .green = 106, .blue = 108, .alpha = 255},
-      PixelColor{.red = 93, .green = 46, .blue = 70, .alpha = 255},
-      PixelColor{.red = 181, .green = 141, .blue = 182, .alpha = 255},
-      PixelColor{.red = 197, .green = 175, .blue = 160, .alpha = 255},
-      PixelColor{.red = 233, .green = 188, .blue = 183, .alpha = 255},
-      PixelColor{.red = 171, .green = 117, .blue = 107, .alpha = 255},
-      PixelColor{.red = 0, .green = 129, .blue = 175, .alpha = 255},
-      PixelColor{.red = 0, .green = 171, .blue = 231, .alpha = 255},
-      PixelColor{.red = 45, .green = 199, .blue = 255, .alpha = 255},
-      PixelColor{.red = 6, .green = 7, .blue = 14, .alpha = 255},
-      PixelColor{.red = 41, .green = 82, .blue = 74, .alpha = 255},
-      PixelColor{.red = 148, .green = 161, .blue = 135, .alpha = 255},
-      PixelColor{.red = 147, .green = 172, .blue = 181, .alpha = 255},
-      PixelColor{.red = 150, .green = 197, .blue = 247, .alpha = 255},
-      PixelColor{.red = 169, .green = 211, .blue = 255, .alpha = 255},
-      PixelColor{.red = 242, .green = 244, .blue = 255, .alpha = 255},
-      PixelColor{.red = 234, .green = 210, .blue = 234, .alpha = 255},
-      PixelColor{.red = 234, .green = 186, .blue = 107, .alpha = 255},
-      PixelColor{.red = 138, .green = 176, .blue = 171, .alpha = 255},
-      PixelColor{.red = 62, .green = 80, .blue = 91, .alpha = 255},
-      PixelColor{.red = 38, .green = 65, .blue = 60, .alpha = 255},
-      PixelColor{.red = 26, .green = 29, .blue = 26, .alpha = 255},
-      PixelColor{.red = 3, .green = 18, .blue = 14, .alpha = 255},
+  std::vector<PixelColor> drWhoColors = {
+      PixelColor{.red = 255, .green = 237, .blue = 231, .alpha = 255},
+      PixelColor{.red = 254, .green = 228, .blue = 213, .alpha = 255},
+      PixelColor{.red = 242, .green = 146, .blue = 147, .alpha = 255},
+      PixelColor{.red = 197, .green = 86, .blue = 137, .alpha = 255},
+      PixelColor{.red = 142, .green = 107, .blue = 187, .alpha = 255},
+      PixelColor{.red = 87, .green = 46, .blue = 140, .alpha = 255},
+      PixelColor{.red = 69, .green = 26, .blue = 142, .alpha = 255},
+      PixelColor{.red = 143, .green = 25, .blue = 111, .alpha = 255},
+      PixelColor{.red = 200, .green = 117, .blue = 135, .alpha = 255},
+      PixelColor{.red = 254, .green = 238, .blue = 215, .alpha = 255},
+      PixelColor{.red = 146, .green = 115, .blue = 182, .alpha = 255},
+      PixelColor{.red = 178, .green = 96, .blue = 180, .alpha = 255},
+    
   };
 
   std::vector<PixelColor> colors = {};
   const int colorsBetween = 32;
 
-  for (unsigned int i = 0; i < rollingInTheDeepColors.size(); i++) {
+  for (unsigned int i = 0; i < drWhoColors.size(); i++) {
     unsigned int compareI = i + 1;
-    colors.push_back(rollingInTheDeepColors[i]);
-    if (compareI >= rollingInTheDeepColors.size()) {
+    colors.push_back(drWhoColors[i]);
+    if (compareI >= drWhoColors.size()) {
       compareI = 0;
     }
     for (int k = 1; k < colorsBetween; k++) {
       PixelColor intermediateColor;
       intermediateColor.red =
-          static_cast<unsigned char>(rollingInTheDeepColors[i].red +
-                                     (rollingInTheDeepColors[compareI].red -
-                                      rollingInTheDeepColors[i].red) *
+          static_cast<unsigned char>(drWhoColors[i].red +
+                                     (drWhoColors[compareI].red -
+                                      drWhoColors[i].red) *
                                          k / colorsBetween);
       intermediateColor.green =
-          static_cast<unsigned char>(rollingInTheDeepColors[i].green +
-                                     (rollingInTheDeepColors[compareI].green -
-                                      rollingInTheDeepColors[i].green) *
+          static_cast<unsigned char>(drWhoColors[i].green +
+                                     (drWhoColors[compareI].green -
+                                      drWhoColors[i].green) *
                                          k / colorsBetween);
       intermediateColor.blue =
-          static_cast<unsigned char>(rollingInTheDeepColors[i].blue +
-                                     (rollingInTheDeepColors[compareI].blue -
-                                      rollingInTheDeepColors[i].blue) *
+          static_cast<unsigned char>(drWhoColors[i].blue +
+                                     (drWhoColors[compareI].blue -
+                                      drWhoColors[i].blue) *
                                          k / colorsBetween);
       intermediateColor.alpha =
-          static_cast<unsigned char>(rollingInTheDeepColors[i].alpha +
-                                     (rollingInTheDeepColors[compareI].alpha -
-                                      rollingInTheDeepColors[i].alpha) *
+          static_cast<unsigned char>(drWhoColors[i].alpha +
+                                     (drWhoColors[compareI].alpha -
+                                      drWhoColors[i].alpha) *
                                          k / colorsBetween);
       colors.push_back(intermediateColor);
     }
